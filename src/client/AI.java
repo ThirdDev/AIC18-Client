2781ec01525d8669de495431c14e18857cecfa6b
Mahdi <mghiasi75@gmail.com>
2018-02-11 22:33:30 +0330

    Fixed scoreSurvivedTurnsCoeff value

--- src/client/AI.java
+++ src/client/AI.java
@@ -38,7 +38,7 @@ public class AI {
     final double scoreCostCoeff = 100.0;
     final double scoreHealthCoeff = -20.0;
     final double scoreHeatMapCoeff = -20.0;
-    final double scoreSurvivedTurnsCoeff = 1.0;
+    final double scoreSurvivedTurnsCoeff = -2.0;
 
     final double defenceBudgetCoeff = 0.5;
     final double avoidSpendingTooMuchMoneyTooSoonCoeff = 1.0;
