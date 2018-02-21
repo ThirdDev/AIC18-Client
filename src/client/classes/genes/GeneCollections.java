e8a51b3dae1855b06b55b3dabf7a9a0b08797c92
Mahdi <mghiasi75@gmail.com>
2018-02-22 02:00:05 +0330

    More GeneCollections

--- src/client/classes/genes/GeneCollections.java
+++ src/client/classes/genes/GeneCollections.java
@@ -24,7 +24,8 @@ public class GeneCollections {
     public FullStateGeneCollection SingleTowerDamage500;
     public FullStateGeneCollection SingleTowerExplore150;
 
-
+    public CountStateGeneCollection RandomDamage2500;
+    public CountStateGeneCollection RandomExplore1000;
 
     //Singleton class
     private GeneCollections() {
@@ -41,13 +42,16 @@ public class GeneCollections {
         TwoDoubleTowersDamage700 = new FullStateGeneCollection("TwoDoubleTowers-DamagePolicy 700-pathLength 40-2018-20-2--19-19-12.sgdf");
         TwoDoubleTowersExplore200 = new FullStateGeneCollection("TwoDoubleTowers-ExplorePolicy 200-pathLength 40-2018-20-2--19-05-53.sgdf");
 
-        //TwoTowersDamage275 = new FullStateGeneCollection("");
+        TwoTowersDamage275 = new FullStateGeneCollection("TwoTowers-DamagePolicy 275-pathLength 100-2018-21-2--22-17-54.sgdf");
         TwoTowersDamage500 = new FullStateGeneCollection("TwoTowers-DamagePolicy 500-pathLength 100-2018-21-2--13-25-39.sgdf");
-        //TwoTowersExplore200 = new FullStateGeneCollection("");
+        TwoTowersExplore200 = new FullStateGeneCollection("TwoTowers-ExplorePolicy 200-pathLength 100-2018-21-2--23-49-39.sgdf");
 
         SingleTowerDamage200 = new FullStateGeneCollection("SingleTower-DamagePolicy 200-pathLength 100-2018-21-2--17-26-28.sgdf");
-        //SingleTowerDamage500 = new FullStateGeneCollection("");
+        SingleTowerDamage500 = new FullStateGeneCollection("SingleTower-DamagePolicy 500-pathLength 100-2018-21-2--22-03-37.sgdf");
         SingleTowerExplore150 = new FullStateGeneCollection("SingleTower-ExplorePolicy 150-pathLength 100-2018-21-2--19-20-24.sgdf");
+
+        RandomDamage2500 = new CountStateGeneCollection("RandomTowers-DamagePolicy 2500-pathLength 20-2018-21-2--01-47-18.sgdf");
+        RandomExplore1000 = new CountStateGeneCollection("RandomTowers-ExplorePolicy 1000-pathLength 20-2018-21-2--01-47-39.sgdf");
 
         long delta = System.currentTimeMillis() - time;
         System.out.println(delta + " ms");
