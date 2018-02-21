fcea6627eb8ac0db3a77ebbdcad127d1faa1f925
Mahdi <mghiasi75@gmail.com>
2018-02-21 14:54:45 +0330

    Simple logger class

---
+++ src/client/classes/Logger.java
@@ -0,0 +1,15 @@
+package client.classes;
+
+public class Logger {
+
+    //Singleton class
+    private Logger() { }
+
+    public static void print(String s) {
+        System.out.print(s);
+    }
+
+    public static void println(String s) {
+        System.out.println(s);
+    }
+}
