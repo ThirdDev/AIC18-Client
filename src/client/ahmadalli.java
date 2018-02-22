13fc518e6b278c6a513f32475c21d3e26d2044e2
ahmadali shafiee <mail@ahmadalli.net>
2018-02-23 02:08:20 +0330

    sort cells by their covered troop and road cells

--- src/client/ahmadalli.java
+++ src/client/ahmadalli.java
@@ -4,15 +4,34 @@ import client.classes.Bank;
 import client.classes.BankAccount;
 import client.model.*;
 
+import java.util.Comparator;
 import java.util.Random;
+import java.util.stream.Stream;
 
 public class ahmadalli {
+
+    private static Comparator<Cell> compareByTroopAndRoadCellCount(World world) {
+        return Comparator.comparingInt(o -> cellScore(o, world));
+    }
+
+    public static int cellScore(Cell cell, World world) {
+        return getNearbyRoadCells(cell, world).
+                mapToInt(x -> (int) x.getUnits().stream().count() + 1).sum();
+    }
+
+    public static Stream<RoadCell> getNearbyRoadCells(Cell cell, World world) {
+        return Util.radiusCells(cell, 2, world.getDefenceMap()).stream()
+                .filter(x -> x instanceof RoadCell)
+                .map(x -> (RoadCell) x);
+    }
+
     public static void plantRandomTowerInASidewayCell(World world) {
         GrassCell[] sidewayCells = world.getDefenceMapPaths().stream()
                 .flatMap(x -> x.getRoad().stream())
-                .flatMap(x -> Util.radiusCells(x, 1, world.getDefenceMap()).stream())
+                .flatMap(x -> Util.radiusCells(x, 2, world.getDefenceMap()).stream())
                 .filter(x -> x instanceof GrassCell)
                 .distinct()
+                .sorted(compareByTroopAndRoadCellCount(world))
                 .toArray(GrassCell[]::new);
 
         GrassCell randomSideWayCell = sidewayCells[rnd.nextInt(sidewayCells.length)];
