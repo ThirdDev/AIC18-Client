ce6d1b1ccc78a2f51a01fdbfa403bd7ada48dad3
ahmadali shafiee <mail@ahmadalli.net>
2018-02-23 02:17:12 +0330

    build in the most valuable cell

--- src/client/ahmadalli.java
+++ src/client/ahmadalli.java
@@ -36,7 +36,14 @@ public class ahmadalli {
                 .sorted(compareByTroopAndRoadCellCount(world))
                 .toArray(GrassCell[]::new);
 
-        GrassCell randomSideWayCell = sidewayCells[rnd.nextInt(sidewayCells.length)];
+
+        GrassCell cellToBuild = null;
+
+        // GrassCell randomSideWayCell = sidewayCells[rnd.nextInt(sidewayCells.length)];
+        // cellToBuild = randomSideWayCell;
+
+        cellToBuild = sidewayCells[sidewayCells.length - 1];
+
         BankAccount defendAccount = null;
         try {
             defendAccount = Bank.getAccount(BankController.BANK_ACCOUNT_DEFENCE);
@@ -49,14 +56,14 @@ public class ahmadalli {
         if (towerType == 0 && defendAccount.canSpend(ArcherTower.INITIAL_PRICE)) {
             try {
                 defendAccount.retrieveMoney(ArcherTower.INITIAL_PRICE);
-                world.createArcherTower(level, randomSideWayCell.getLocation().getX(), randomSideWayCell.getLocation().getX());
+                world.createArcherTower(level, cellToBuild.getLocation().getX(), cellToBuild.getLocation().getX());
             } catch (Exception ex) {
             }
         }
         if (towerType == 1 && defendAccount.canSpend(CannonTower.INITIAL_PRICE)) {
             try {
                 defendAccount.retrieveMoney(CannonTower.INITIAL_PRICE);
-                world.createCannonTower(level, randomSideWayCell.getLocation().getX(), randomSideWayCell.getLocation().getX());
+                world.createCannonTower(level, cellToBuild.getLocation().getX(), cellToBuild.getLocation().getX());
             } catch (Exception ex) {
             }
         }
