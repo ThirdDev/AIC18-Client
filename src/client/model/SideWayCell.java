cc9b2d58dad03e308734fced0b6157400385c703
Amirhossein Mirtabatabaeipour <mirtaba@outlook.com>
2018-02-23 02:33:43 +0330

    new Defence system is up :D

--- src/client/model/SideWayCell.java
+++ src/client/model/SideWayCell.java
@@ -2,16 +2,18 @@ package client.model;
 
 import java.util.ArrayList;
 
-public class SideWayCell extends Cell {
+public class SideWayCell extends GrassCell implements Comparable {
 
     ArrayList<Path> paths;
     ArrayList<RoadCell> roadCells;
 
-    public SideWayCell(int x, int y) {
-        super(x, y);
+    public SideWayCell(int x, int y, Tower tower) {
+        super(x, y, tower);
         paths = new ArrayList<>();
         roadCells = new ArrayList<>();
     }
+
+
 
     public ArrayList<Path> getPaths() {
         return paths;
@@ -28,4 +30,14 @@ public class SideWayCell extends Cell {
     public void addRoadCells(RoadCell roadCell) {
         this.roadCells.add(roadCell);
     }
+
+    @Override
+    public int compareTo(Object o) {
+        SideWayCell oo = (SideWayCell) o;
+        int ans = roadCells.size() - oo.getRoadCells().size();
+        if(ans == 0) ans = paths.size() - oo.getPaths().size();
+        return ans;
+    }
+
+
 }
