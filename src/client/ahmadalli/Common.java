0d60fd8d527f8f32eff7fd0f4833a84f6a9490ce
ahmadali shafiee <mail@ahmadalli.net>
2018-03-07 11:02:47 +0330

    bean check

--- src/client/ahmadalli/Common.java
+++ src/client/ahmadalli/Common.java
@@ -1,6 +1,7 @@
 package client.ahmadalli;
 
 import client.Util;
+import client.classes.Logger;
 import client.model.*;
 
 import java.util.ArrayList;
@@ -8,5 +9,20 @@ import java.util.stream.Stream;
 
 public class Common {
     public static void initialize(World world) {
+    }
+
+    public static void beanCheck(World world) {
+        for (BeanEvent beanEvent : world.getBeansInThisTurn()) {
+            Cell beanned;
+            if (beanEvent.getOwner() == Owner.ENEMY)
+                beanned = world.getDefenceMap().getCell(beanEvent.getPoint().getX(), beanEvent.getPoint().getY());
+            else {
+                beanned = world.getAttackMap().getCell(beanEvent.getPoint().getX(), beanEvent.getPoint().getY());
+            }
+
+            if (!(beanned instanceof BlockCell)) {
+                Logger.error("fuck fuck fuck beans on map aren't right");
+            }
+        }
     }
 }
