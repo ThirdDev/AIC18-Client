84126f0f1ba81e28b842655b6ac716e1457f4715
ahmadali shafiee <mail@ahmadalli.net>
2018-03-08 01:12:30 +0330

    log time passed for each turn and task

--- src/client/AI.java
+++ src/client/AI.java
@@ -7,6 +7,7 @@ import client.classes.Bank;
 import client.classes.Logger;
 import client.classes.genes.GeneCollections;
 import client.model.*;
+import common.util.Log;
 
 import java.util.*;
 
@@ -39,27 +40,71 @@ public class AI {
 
     //This function will be called on both simple and complex turns
     private void commonTurnFunctions(World game) {
-        if (defence == null) defence = new Defence(
-                Bank.getAccount(BankController.BANK_ACCOUNT_DEFENCE),
-                game
-        );
         Logger.println("Turn " + game.getCurrentTurn());
+
+        long turnInitMilliseconds = System.currentTimeMillis();
+        long taskInitMilliseconds = System.currentTimeMillis();
+
+        if (defence == null) {
+            taskInitMilliseconds = System.currentTimeMillis();
+            defence = new Defence(
+                    Bank.getAccount(BankController.BANK_ACCOUNT_DEFENCE),
+                    game
+            );
+            logTaskTime("amirhosein's defence initialization", taskInitMilliseconds, turnInitMilliseconds);
+        }
+
+        taskInitMilliseconds = System.currentTimeMillis();
         BankController.handleMoney(game.getMyInformation());
+        logTaskTime("handling money", taskInitMilliseconds, turnInitMilliseconds);
+
+        taskInitMilliseconds = System.currentTimeMillis();
         Common.initialize(game);
+        logTaskTime("ahmadalli's initialize", taskInitMilliseconds, turnInitMilliseconds);
 
         Logger.print("Attack budget: " + Bank.getAccount(BankController.BANK_ACCOUNT_ATTACK).getBalance());
         Logger.print(", Defence budget: " + Bank.getAccount(BankController.BANK_ACCOUNT_DEFENCE).getBalance());
         Logger.println(", Total: " + game.getMyInformation().getMoney());
         Logger.println(Bank.getAccount(BankController.BANK_ACCOUNT_ATTACK).getPercent() + ", " + Bank.getAccount(BankController.BANK_ACCOUNT_DEFENCE).getPercent());
 
+        taskInitMilliseconds = System.currentTimeMillis();
         defence.run(game);
-        //TowerCreation.simpleTowerCreation(game);
+        logTaskTime("amirhosein's defence", taskInitMilliseconds, turnInitMilliseconds);
+
+        // taskInitMilliseconds = System.currentTimeMillis();
+        // TowerCreation.simpleTowerCreation(game);
+        // logTaskTime("ahmadalli's defence", taskInitMilliseconds, turnInitMilliseconds);
+
+        taskInitMilliseconds = System.currentTimeMillis();
         StormCreation.stormIfNecessary(game);
+        logTaskTime("ahmadalli's storm", taskInitMilliseconds, turnInitMilliseconds);
 
+        taskInitMilliseconds = System.currentTimeMillis();
         Attack.Attack(game);
+        logTaskTime("attack", taskInitMilliseconds, turnInitMilliseconds);
 
+        taskInitMilliseconds = System.currentTimeMillis();
         BankController.updateBudgetDistribution(game);
+        logTaskTime("updating bank's budget distribution", taskInitMilliseconds, turnInitMilliseconds);
+
+        taskInitMilliseconds = System.currentTimeMillis();
         MorningBeams(game);
+        logTaskTime("morning beans", taskInitMilliseconds, turnInitMilliseconds);
+
+        long totalPassed = (System.currentTimeMillis() - turnInitMilliseconds);
+        Logger.println("turn passed in " + totalPassed + "ms");
+
+        if ((game.getCurrentTurn() % 10 != 0 && totalPassed > 200) || (game.getCurrentTurn() % 10 == 0 && totalPassed > 1000)) {
+            Logger.error("golam leftesh dadi server bikhial shold. bi haya!");
+        }
+
+        Logger.println("===== end of turn " + game.getCurrentTurn() + "=====");
+        Logger.println("");
+    }
+
+    private void logTaskTime(String taskName, long taskInitMilliseconds, long turnInitMilliseconds) {
+        Logger.println(">>>>> " + taskName + " took " + (System.currentTimeMillis() - taskInitMilliseconds) + "ms" +
+                " and passed " + (System.currentTimeMillis() - turnInitMilliseconds) + "ms in turn <<<<<");
     }
 
     //int beansCount = 0;
