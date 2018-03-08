946bd23ce1668281811d326b35551bc9756e9a27
ahmadali shafiee <mail@ahmadalli.net>
2018-03-08 08:02:23 +0330

    storm creation logs

--- src/client/ahmadalli/defend/StormCreation.java
+++ src/client/ahmadalli/defend/StormCreation.java
@@ -9,19 +9,24 @@ public class StormCreation {
     public static void stormIfNecessary(World world) {
         if (world.getMyInformation().getStormsLeft() == 0)
             return;
+        Logger.println("starting storm if necessary");
 
         RoadCell[] dangerousInRange3Ordered = client.ahmadalli.utils.RoadCell.orderByDamageScoreAscending(
                 world.getDefenceMapPaths(),
                 0.2, 3, 3);
-        if (dangerousInRange3Ordered.length == 0)
+        if (dangerousInRange3Ordered.length == 0) {
+            Logger.println("no dangerous cell found in range 3 of the end of the map");
             return;
+        }
 
         RoadCell mostDangerous = dangerousInRange3Ordered[0];
         Cell bestShot = client.ahmadalli.utils.RoadCell.centerOfMostDangerousAreaContainingRoadCell(mostDangerous, world.getDefenceMap(), 2);
         int bestShotScore = client.ahmadalli.scoring.Cell.stormCoveredDamageScore(bestShot, world.getDefenceMap(), 2);
         int mostDangerousScore = client.ahmadalli.scoring.RoadCell.damageToBaseScore(mostDangerous);
-        Logger.println("bestShotScore = " + bestShotScore + ", mostDangerousScore = " + mostDangerousScore);
+        Logger.println("bestShotScore: " + bestShotScore + ", mostDangerousScore: " + mostDangerousScore +
+                ", current strength: " + world.getMyInformation().getStrength());
         if ((bestShotScore >= 8) || (mostDangerousScore >= world.getMyInformation().getStrength())) {
+            Logger.println("storm is necessary. storming @(" + bestShot.getLocation().getX() + ", " + bestShot.getLocation().getY());
             world.createStorm(bestShot.getLocation().getX(), bestShot.getLocation().getY());
         }
 
